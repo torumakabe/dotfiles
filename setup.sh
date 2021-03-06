77a25b5adf2a0964f10bb9fdb87ea55e510b10a4
Toru Makabe <tomakabe@microsoft.com>
2021-03-06 09:46:39 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,7 +15,7 @@ for file in $files; do
 done
 
 # Install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Updating package lists..."
 brew update
