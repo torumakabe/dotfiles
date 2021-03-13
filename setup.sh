ded3cd7961bd206b05490144bb7fdf3b95b18243
Toru Makabe <tomakabe@microsoft.com>
2021-03-13 15:00:37 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -103,7 +103,6 @@ sudo apt-get install jq -y
 sudo apt-get install python3 python3-venv -y
 sudo apt-get install python3-pip -y
 sudo apt-get install htop -y
-sudo apt-get install direnv -y
 sudo apt-get install peco -y
 
 # node/n Install
