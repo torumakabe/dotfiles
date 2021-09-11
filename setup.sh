7e3e56e05ba7a26c88245ba05140b7eb75220832
ToruMakabe <tomakabe@microsoft.com>
2021-09-11 14:49:37 +0900

    feat & refactor: add jump

--- setup.sh
+++ setup.sh
@@ -7,16 +7,18 @@ files="bash_profile zshrc gitconfig"
 git clone https://github.com/ToruMakabe/dotfiles.git ~/dotfiles
 
 mkdir -p $olddir
-cd $dir
+cd $dir || exit
 
 for file in $files; do
-    if [ -f ~/.$file ]; then
-      mv ~/.$file ~/dotfiles_old/
+    if [ -f ~/."$file" ]; then
+      mv ~/."$file" ~/dotfiles_old/
     fi
-    ln -s $dir/$file ~/.$file
+    ln -s $dir/"$file" ~/."$file"
 done
 
-sudo chsh -s /usr/bin/zsh codespace
+echo ''
+echo "Now installing jump..."
+wget https://github.com/gsamokovarov/jump/releases/download/v0.40.0/jump_0.40.0_amd64.deb && sudo dpkg -i jump_0.40.0_amd64.deb
 
 echo ''
 echo 'Setup completed!'
