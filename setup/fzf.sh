f71fcb29a748bf987fc41848dc4ce115765967c0
ToruMakabe <tomakabe@microsoft.com>
2021-09-12 11:13:02 +0900

    fix: check repo

--- setup/fzf.sh
+++ setup/fzf.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-apt install fzf
+apt_get_update_if_needed()
+{
+    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update
+    else
+        echo "Skipping apt-get update."
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt_get_update_if_needed
+apt-get -y install --no-install-recommends fzf
