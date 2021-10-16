ce333a4b967eaf122c7fefc9281410befee2871f
Toru Makabe <tomakabe@microsoft.com>
2021-10-16 22:52:41 +0900

    Update github-cli.sh

--- setup/github-cli.sh
+++ setup/github-cli.sh
@@ -10,3 +10,6 @@ apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
 apt-add-repository https://cli.github.com/packages
 apt update
 apt install -y gh
+
+gh extension install https://github.com/cappyzawa/gh-ghq-cd
+gh alias set cd ghq-cd
