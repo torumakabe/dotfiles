61819c31da89d0750cc4131491e9d600b4426e1d
ToruMakabe <tomakabe@microsoft.com>
2022-02-01 10:48:13 +0900

    feat: add pb & grpc setup

---
+++ setup/grpc.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+export DEBIAN_FRONTEND=noninteractive
+
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
---
+++ setup/pb.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+apt_get_update_if_needed()
+{
+    if [ ! -d "/var/lib/apt/lists" ] || [ "$(ls /var/lib/apt/lists/ | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update
+    else
+        echo "Skipping apt-get update."
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+apt_get_update_if_needed
+apt install -y protobuf-compiler
