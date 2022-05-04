7059a5e60497664670b4d221f43ef7beb42bcdfb
Toru Makabe <tomakabe@microsoft.com>
2022-05-04 10:03:52 +0900

    fix: remove unneeded line feed

--- setup/docker.sh
+++ setup/docker.sh
@@ -27,9 +27,7 @@ esac
 
 apt-get -y install --no-install-recommends ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo 
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 usermod -aG docker "${SUDO_USER}"
