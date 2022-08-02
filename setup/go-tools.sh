1e27d7f7a8f374acc1989c0c4fa62849179841c7
Toru Makabe <tomakabe@microsoft.com>
2022-08-03 08:03:15 +0900

    fix: golangci-lint

--- setup/go-tools.sh
+++ setup/go-tools.sh
@@ -5,9 +5,9 @@ export DEBIAN_FRONTEND=noninteractive
 
 echo "Installing golangci-lint..."
 # binary will be $(go env GOPATH)/bin/golangci-lint
-curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin"
+curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin
 
-golangci-lint --version
+golangci-lint version
 
 echo "Installing fieldalignment..."
 go install golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment@latest
