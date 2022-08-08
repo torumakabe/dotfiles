d7a8baa8c709fdb2c261b14809e94124f4d78e33
Toru Makabe <tomakabe@microsoft.com>
2022-08-08 13:15:00 +0900

    fix: gpg import way

--- setup/trivy.sh
+++ setup/trivy.sh
@@ -4,7 +4,7 @@
 set -eo pipefail
 
 sudo apt-get install wget apt-transport-https gnupg lsb-release
-wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo gpg --no-default-keyring --keyring /etc/apt/trusted.gpg.d/trivy.gpg --import
+wget -qO - https://aquasecurity.github.io/trivy-repo/deb/public.key | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/trivy.gpg
 echo deb https://aquasecurity.github.io/trivy-repo/deb $(lsb_release -sc) main | sudo tee /etc/apt/sources.list.d/trivy.list
 sudo apt-get update
 sudo apt-get install trivy
