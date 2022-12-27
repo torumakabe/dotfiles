5cc04740151705d19459e0d9cc370807a8ec613d
Toru Makabe <993850+torumakabe@users.noreply.github.com>
2022-12-27 18:03:28 +0900

    feat: add buf

--- setup/grpc.sh
+++ setup/grpc.sh
@@ -6,3 +6,4 @@ export DEBIAN_FRONTEND=noninteractive
 
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install github.com/bufbuild/buf/cmd/buf@latest
