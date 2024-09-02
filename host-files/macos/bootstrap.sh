6be42cab2dce0395da9a09898c6c6284f95420fa
Toru Makabe <993850+torumakabe@users.noreply.github.com>
2024-09-02 20:55:09 +0900

    Update bootstrap.sh

--- host-files/macos/bootstrap.sh
+++ host-files/macos/bootstrap.sh
@@ -25,6 +25,8 @@ brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
 brew install --cask devtoys
 brew install --cask dotnet-sdk
+brew tap microsoft/dev-proxy
+brew install dev-proxy
 
 echo ''
 echo 'Setup completed!'
