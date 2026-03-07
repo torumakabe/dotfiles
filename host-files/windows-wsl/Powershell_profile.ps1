76e39c5d112161a937559b26a76200276ad5570a
Toru Makabe <993850+torumakabe@users.noreply.github.com>
2026-03-07 09:33:27 +0900

    Enhance PowerShell profile with functions and aliases

--- host-files/windows-wsl/Powershell_profile.ps1
+++ host-files/windows-wsl/Powershell_profile.ps1
@@ -1,4 +1,41 @@
+# === Environment variables ===
 $env:EDITOR = 'edit'
+
+# === Functions ===
+
+# Copilot CLI workaround: edit.exe を新コンソールで開く短縮コマンド
+function Open-EditInNewConsole {
+    param(
+        [Parameter(ValueFromRemainingArguments = $true)]
+        [string[]]$Path
+    )
+
+    $escapedPath = $Path | ForEach-Object {
+        '"' + ($_ -replace '"', '\"') + '"'
+    }
+    $command = if ($escapedPath) {
+        'start "" edit ' + ($escapedPath -join ' ')
+    }
+    else {
+        'start "" edit'
+    }
+
+    Start-Process -FilePath 'cmd.exe' -ArgumentList '/c', $command | Out-Null
+}
+
+function zi { cdi @args }
+
+# ghq + fzf: fuzzy-search git repositories and cd into selection
+function ghcd {
+    $dir = ghq list -p | fzf
+    if ($dir) { Set-Location $dir }
+}
+
+# === Aliases ===
+Set-Alias -Name e -Value Open-EditInNewConsole
+Set-Alias -Name k -Value kubectl
+
+# === Completers and tool setup ===
 
 # https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-windows?tabs=azure-cli#enable-tab-completion-in-powershell
 Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
@@ -19,17 +56,9 @@ Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
     Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
 }
 
-Set-Alias -Name k -Value kubectl
 kubectl completion powershell | Out-String | Invoke-Expression
 Register-ArgumentCompleter -CommandName k -ScriptBlock $__kubectlCompleterBlock
 
 # zoxide (smarter cd)
 Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
 Set-Alias -Name z -Value cd
-function zi { cdi @args }
-
-# ghq + fzf: fuzzy-search git repositories and cd into selection
-function ghcd {
-    $dir = ghq list -p | fzf
-    if ($dir) { Set-Location $dir }
-}
